582c688cc669d7369937d1920733f350436c4b62
mattykuch <bk.advisors.com@gmail.com>
2025-11-05 19:22:23 +0300

    scatterplot

---
+++ simple-scatterplot/src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+  // This is where the logic lives
+  import data from '$data/data.js';
+
+ // console.log(data);
+</script>
+
+<!-- Svelte each block to display data (must be in component markup, not inside <script>) -->
+{#each data as d}
+  <p>{d.name}</p>
+{/each}
