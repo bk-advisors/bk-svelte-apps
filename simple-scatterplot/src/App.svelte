1f12e83ef3f9c2015c2bdfa60d91ab0aa440af77
mattykuch <bk.advisors.com@gmail.com>
2025-11-05 21:49:34 +0300

    3.2 scatterplot complete

--- simple-scatterplot/src/App.svelte
+++ simple-scatterplot/src/App.svelte
@@ -2,10 +2,37 @@
   // This is where the logic lives
   import data from '$data/data.js';
 
- // console.log(data);
+  import { scaleLinear } from 'd3-scale';
+
+// Dimensions
+  let width = 400;
+  let height = 400;
+
+// Scales
+ let xScale = scaleLinear()
+   .domain([0, 100])  // input
+   .range([0, width]);  // output
+
+  let yScale = scaleLinear()
+    .domain([0, 60])  // input
+    .range([height, 0]);  // output
+
 </script>
 
 <!-- Svelte each block to display data (must be in component markup, not inside <script>) -->
+
+<svg width="{width}" height="{height}" style="border:1px solid black;">
+  {#each data as d}
+    <circle 
+    cx="{xScale(d.grade)}" 
+    cy="{yScale(d.hours)}" 
+    r="5" 
+    fill="blue"
+    stroke="black"
+    stoke-width="1" />
+  {/each}
+</svg>
+
 {#each data as d}
-  <p>{d.name}</p>
+  <p>{d.name} studied for {d.hours} hours and scored {d.grade}</p>
 {/each}
