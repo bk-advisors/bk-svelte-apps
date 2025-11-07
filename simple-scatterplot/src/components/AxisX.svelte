7c8f25efc8a5168331c70afcdfeb7e01d5a06482
mattykuch <bk.advisors.com@gmail.com>
2025-11-07 21:36:13 +0300

    Added g class for ticks , then line and text

--- simple-scatterplot/src/components/AxisX.svelte
+++ simple-scatterplot/src/components/AxisX.svelte
@@ -4,7 +4,7 @@
 
 
     // console.log(xScale);
-     console.log(height);  
+    // console.log(height);  
     
     let xTicks = [0, 25, 50, 75, 100];
 
@@ -13,9 +13,10 @@
 
 <g class="axis-x" transform="translate(0, {height})">
 {#each xTicks as tick}
-          
-    <text x="{xScale(tick)}" y={0}>
-        {tick}
-    </text>
+    <g class='tick' transform="translate({xScale(tick)}, 0)">
+      <!-- <line /> -->
+      <line x1="0" x2="0" y1="0" y2="6" stroke="hsla(212, 10%, 53%, 1)" />
+      <text x="0" y="6">{tick}%</text>
+    </g>
 {/each}
 </g>
