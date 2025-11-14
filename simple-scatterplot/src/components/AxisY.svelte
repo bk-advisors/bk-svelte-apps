709b42689218033f6a83a379e4bac13819f8870e
mattykuch <bk.advisors.com@gmail.com>
2025-11-14 22:23:39 +0300

    AxisY line polish

--- simple-scatterplot/src/components/AxisY.svelte
+++ simple-scatterplot/src/components/AxisY.svelte
@@ -10,10 +10,10 @@
 </script>
 
 <g class="axis-y" tranform="translate(0, {width})">
-{#each yTicks as tick}
+{#each yTicks as tick, index}
     <g class='tick' transform="translate(0, {yScale(tick)})">
-      <line x1="0" x2="{width}" y1="0" y2="0" stroke="hsla(212, 10%, 53%, 1)" />
-      <text x="-9" y="0" dy="4" dominant-baseline="middle" text-anchor="end">{tick}</text>
+      <line x1="0" x2="{width}" y1="0" y2="0" stroke={index === 0? '#8f8f8f' : '#e5e7eb'} />
+      <text x="-9" y="-6" dy="4" dominant-baseline="middle" text-anchor="end">{tick}</text>
     </g>
 {/each}
 </g>
