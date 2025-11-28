a41f41873107dfea6cd032333040bf1b8b83ce90
mattykuch <bk.advisors.com@gmail.com>
2025-11-28 20:49:59 +0300

    Nice tooltip 2025.11.28

--- simple-scatterplot/src/components/Tooltip.svelte
+++ simple-scatterplot/src/components/Tooltip.svelte
@@ -13,9 +13,8 @@
 
 {#if hoveredData}
     <div class="tooltip" style="left: {xPos + 50}px; top: {yPos + 20}px; background: white; padding: 8px 6px; pointer-events: none;">
-        <strong>{hoveredData.name}</strong><br />
-        Hours Studied: {hoveredData.hours}<br />
-        Final Grade: {hoveredData.grade}%
+        <h1>{hoveredData.name} <span>{hoveredData.grade}%</span></h1>
+        <h2>{hoveredData.hours} hours studied</h2>
     </div>
 {/if}
 
@@ -27,5 +26,29 @@
     border-radius: 4px; 
 
     }
+
+    h1 {
+    font-size: 1rem;
+    font-weight: 500;
+    margin-bottom: 6px;
+    width: 100%;
+}
+
+    h2 {
+    font-size: 0.8rem;
+    font-weight: 300;
+    text-transform: uppercase;
+}
+
+    span {
+    background: #dda0dd50;
+    font-size: 80%;
+    margin-left: 2px;
+    padding: 2px 4px;
+    display: inline-block;
+    vertical-align: bottom;
+    border-radius: 3px;
+    color: rgba(0, 0, 0, 0.7);
+}
   
 </style>
